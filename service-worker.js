020e003fd9cd143762b6d2851ca8e75bb5f94c4e
varegaz128 <pratamahary20@gmail.com>
2025-10-24 16:17:49 +0700

    baru

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,5 @@
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("stok-cache-v1").then((cache) => {
-      return cache.addAll(["/", "/index.html", "/app.js"]);
-    })
-  );
+  console.log("Service Worker installed");
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("fetch", () => {});
